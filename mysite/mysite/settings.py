5b9ef097e3f089fdd27deb4535821319a50bab6c
Denise Wong <denisewongwaiyan@gmail.com>
2020-07-19 21:52:58 +0800

    deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_heroku
 
 import dj_database_url
 
@@ -170,3 +171,4 @@ POSTMAN_AUTO_MODERATE_AS = True
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+django_heroku.settings(locals())
